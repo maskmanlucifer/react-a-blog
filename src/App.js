f75d1a42bdf0aa47dd6dbfe346e747c98b7a5f35
Aman <maskmanaman@gmail.com>
2021-09-03 17:07:01 +0530

    Link done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter as Router, Switch} from 'react-router-dom'; 
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Create from './Create';
 function App() {
   const title = "Welcome to New Blog";
   return (
+    <Router>
     <div className="App">
       <Navbar/>
       <div className="content">
-        <Home/>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/create">
+            <Create/>
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
