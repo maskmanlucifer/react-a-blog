280b72a3ff37598134c6dd08f19ef43c2190dd25
Aman <maskmanaman@gmail.com>
2021-09-03 13:42:12 +0530

    done with useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,45 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+  const [blogs, setBlogs] = useState(null);
+  
+  // runs on every render 
+  // we can pass a dependency array to specify when useEffect will run
+  
+  const [isPending,setPending] = useState(true);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  const [error,setError] = useState(null);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      fetch('http://localhost:8000/blogs')
+      .then((res)=>{
+        if(!res.ok)
+        {
+          throw Error("could not fetch data for that resource");
+        }
+        return res.json()
+      })
+      .then((data)=>{
+        setBlogs(data);
+        setPending(false);
+        setError(null);
+      })
+      .catch((err)=>{
+        setError(err.message);
+        setPending(false);
+      });
+    },1000);
+    
+  },[]);
+  
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading.....</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 }
