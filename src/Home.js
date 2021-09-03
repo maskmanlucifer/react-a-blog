233796f4f41fa423ca3e1a1ddcd188c3ab79d4e7
Aman <maskmanaman@gmail.com>
2021-09-03 16:48:23 +0530

    cutom hooks finished

--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,8 @@
 import { useState , useEffect} from "react";
 import BlogList from "./BlogList";
-
+import useFetch from "./useFetch";
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  
-  // runs on every render 
-  // we can pass a dependency array to specify when useEffect will run
-  
-  const [isPending,setPending] = useState(true);
-
-  const [error,setError] = useState(null);
-
-  useEffect(()=>{
-    setTimeout(()=>{
-      fetch('http://localhost:8000/blogs')
-      .then((res)=>{
-        if(!res.ok)
-        {
-          throw Error("could not fetch data for that resource");
-        }
-        return res.json()
-      })
-      .then((data)=>{
-        setBlogs(data);
-        setPending(false);
-        setError(null);
-      })
-      .catch((err)=>{
-        setError(err.message);
-        setPending(false);
-      });
-    },1000);
-    
-  },[]);
-  
-
+  const { data:blogs,isPending,error } = useFetch('http://localhost:8000/blogs');
   return (
     <div className="home">
       {error && <div>{error}</div>}
