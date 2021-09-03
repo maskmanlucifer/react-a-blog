5a658cc1208d5e14b3f9e9ba9ea247048bc6a466
Aman <maskmanaman@gmail.com>
2021-09-03 18:04:05 +0530

    form added

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,55 @@
+import { useState } from "react/cjs/react.development";
+
 const Create = () => {
+    const [title,setTitle] = useState('');
+    const [body,setBody] = useState('');
+    const [author,setAuthor] = useState('mario');
+    const [isPending,setPending] = useState(false);
+
+    
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const blog = {title,body,author};
+
+      setPending(true);
+      fetch('http://localhost:8000/blogs',{
+          method : 'POST',
+          headers : {"Content-Type":"application/json"},
+          body : JSON.stringify(blog)
+      })
+      .then(()=>{
+        console.log("New Blog Added");
+        setPending(false);  
+        setTitle('');
+        setBody('');
+      })
+    };
+
     return ( 
         <div className="create">
-            <h1>Add a new blog</h1>
+            <h2>Add a new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input type="text" required value={title} onChange={(e)=>{
+                    setTitle(e.target.value);
+                }}/>
+                <p>{title}</p>
+                <label >Blog body:</label>
+                <textarea required value={body} onChange={(e)=>{
+                    setBody(e.target.value)}}></textarea>
+                <p>{body}</p>
+                <label>Blog author:</label>
+                <select value={author} onChange={(e)=>{
+                   setAuthor(e.target.value);
+                }}>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <p>{author}</p>
+                
+                {!isPending && <button>Add Blog</button> }
+                {isPending && <button disabled>Adding Blog......</button>}
+            </form>
         </div>
      );
 }
